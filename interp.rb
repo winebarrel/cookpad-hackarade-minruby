dee96eebb57b203b1be3febb9b30c665e718f998
winebarrel <sugawara@winebarrel.jp>
2018-10-05 11:23:49 +0900

    implement test3-1

--- interp.rb
+++ interp.rb
@@ -24,6 +24,8 @@ def evaluate(exp, env)
     evaluate(exp[1], env) / evaluate(exp[2], env)
   when "%"
     evaluate(exp[1], env) % evaluate(exp[2], env)
+  when ">"
+    evaluate(exp[1], env) > evaluate(exp[2], env)
   # ... Implement other operators that you need
 
 
@@ -53,16 +55,11 @@ def evaluate(exp, env)
 #
 
   when "if"
-    # Branch.  It evaluates either exp[2] or exp[3] depending upon the
-    # evaluation result of exp[1],
-    #
-    # Advice:
-    #   if ???
-    #     ???
-    #   else
-    #     ???
-    #   end
-    raise(NotImplementedError) # Problem 3
+    if evaluate(exp[1], env)
+      evaluate(exp[2], env)
+    else
+      evaluate(exp[3], env)
+    end
 
   when "while"
     # Loop.
